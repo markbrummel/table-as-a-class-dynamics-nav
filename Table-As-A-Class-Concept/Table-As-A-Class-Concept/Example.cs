72d893ac01d4b2dc55f013ab96d502925623f6ba
Mark Brummel <mark.brummel@outlook.com>
2014-11-26 16:22:49 +0000

    First Concept, Customer Class

---
+++ Table-As-A-Class-Concept/Table-As-A-Class-Concept/Example.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Customer
+{
+    public string No_ { get; set; }
+    public string Name { get; set; }
+    public string SearchName { get; set; }
+    public string Address { get; set; }
+    //...
+    public float CreditLimitLCY { get; set; }
+    //...
+    public int StatisticsGroup { get; set; }
+
+    public string formatAddress()
+    {
+        return ( Address );
+    }
+	
+}
