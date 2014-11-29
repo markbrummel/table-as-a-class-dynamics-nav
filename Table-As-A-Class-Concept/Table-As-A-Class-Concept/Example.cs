556fc335292ced750022150c88fc385da9d57df9
Mark Brummel <mark.brummel@outlook.com>
2014-11-29 09:47:35 +0000

    Version of orriginal blog

--- Table-As-A-Class-Concept/Table-As-A-Class-Concept/Example.cs
+++ Table-As-A-Class-Concept/Table-As-A-Class-Concept/Example.cs
@@ -2,7 +2,15 @@
 
 public class Customer
 {
-    public string No_ { get; set; }
+    public string No_
+    {
+        get
+        {
+            NoSeriesManagement test = new NoSeriesManagement();
+            return test.GetNextNo("");;
+        }
+        set { No_ = value; }
+    }
     public string Name { get; set; }
     public string SearchName { get; set; }
     public string Address { get; set; }
@@ -11,9 +19,103 @@ public class Customer
     //...
     public int StatisticsGroup { get; set; }
 
-    public string formatAddress()
+    public string[] formatAddress()
     {
-        return ( Address );
+        FormatAddress FormatAddress = new FormatAddress();
+        return(FormatAddress.formatCustomer(this));
     }
 	
+}
+
+public class SalesHeader
+{
+    public int DocumentType { get; set; }
+    public string SelltoCustomerNo_ { get; set; }
+    public string No_ { get; set; }
+    public string YourReference { get; set; }
+    public DateTime OrderDate 
+    {
+        get { return PostingDate;}
+        set { PostingDate = value; } 
+    }
+    public DateTime PostingDate { get; set; }
+    public string SelltoCustomerName 
+    { 
+        get; 
+        set; 
+    }
+    public string SelltoAddress { get; set; }
+    
+    public void OnValidateSelltoCustomerNo_ (Customer Customer)
+    {
+        SelltoCustomerName = Customer.Name;
+        SelltoAddress = Customer.Address;
+    }
+
+    public SalesInvoice Post()
+    {
+        SalesPost SalesPost = new SalesPost();
+        return(SalesPost.OnRun(this));
+    }
+}
+
+public class SalesInvoice
+{
+    public string SelltoCustomerNo_ { get; set; }
+    public string No_ { get; set; }
+    public string YourReference { get; set; }
+    public DateTime OrderDate { get; set; }
+    public DateTime PostingDate { get; set; }
+    public string SelltoCustomerName { get; set; }
+    public string SelltoAddress { get; set; }
+    
+    public string[] formatAddress()
+    {
+        FormatAddress FormatAddress = new FormatAddress();
+        return (FormatAddress.formatSalesInvoice(this));
+    }
+}
+
+public class SalesPost 
+{
+    public SalesInvoice OnRun(SalesHeader SalesHeader)
+    {
+        SalesInvoice SalesInvoice = new SalesInvoice();
+        SalesInvoice.No_ = SalesHeader.No_;
+        SalesInvoice.SelltoCustomerNo_ = SalesHeader.SelltoCustomerNo_;
+        SalesInvoice.YourReference = SalesHeader.YourReference;
+        SalesInvoice.OrderDate = SalesHeader.OrderDate;
+        SalesInvoice.PostingDate = SalesHeader.PostingDate;
+        SalesInvoice.SelltoCustomerName = SalesHeader.SelltoCustomerName;
+        SalesInvoice.SelltoAddress = SalesHeader.SelltoAddress;
+        return (SalesInvoice);
+    }
+}
+
+public class FormatAddress
+{
+    public string[] formatCustomer (Customer Customer)
+    {
+        return (formatAddr(Customer.Name, Customer.Address));
+    }
+    public string[] formatSalesInvoice(SalesInvoice SalesInvoice)
+    {
+        return (formatAddr(SalesInvoice.SelltoCustomerName, SalesInvoice.SelltoAddress));
+    }
+    private string[] formatAddr(string Name, string Address)
+    {
+        string[] AddrArray;
+        AddrArray = new string[7];
+        AddrArray[0] = Name;
+        AddrArray[1] = Address;
+        return (AddrArray);
+    }
+}
+
+public class NoSeriesManagement
+{
+    public string GetNextNo(string NoSeriesCode)
+    {
+        return ("C00010");
+    }
 }
--- Table-As-A-Class-Concept/Table-As-A-Class-Concept/Program.cs
+++ Table-As-A-Class-Concept/Table-As-A-Class-Concept/Program.cs
@@ -10,6 +10,44 @@ namespace Table_As_A_Class_Concept
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Create the Customer Record..............");
+
+            Customer Cust = new Customer();
+            Cust.Name = "Mark Brummel";
+            Cust.Address = "Somewhere";
+            
+            Console.WriteLine(Cust.No_);
+
+            Console.WriteLine("Return the Customer Address..............");
+
+            foreach (string Addr in Cust.formatAddress())
+                Console.WriteLine(Addr);
+
+            Console.ReadKey();
+
+            Console.WriteLine("Create the Sales Header..............");
+
+            SalesHeader SalesHdr = new SalesHeader();
+            SalesHdr.SelltoCustomerNo_ = Cust.No_;
+            SalesHdr.OnValidateSelltoCustomerNo_(Cust);
+            SalesHdr.PostingDate = new DateTime(2014, 11, 27);
+
+            Console.WriteLine("Show Customer Name..............");
+
+            Console.WriteLine(SalesHdr.SelltoCustomerName);
+            Console.WriteLine(SalesHdr.OrderDate.ToString());
+
+            Console.WriteLine("Post Sales Header..............");
+
+            SalesInvoice SalesInv = SalesHdr.Post();
+
+            Console.WriteLine("Print Address Sales Invoice..............");
+            
+            foreach (string Addr in SalesInv.formatAddress())
+                Console.WriteLine(Addr);
+
+            Console.ReadKey();
         }
     }
 }
